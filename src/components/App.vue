d5ec750513216aac07ec8a3d9eeaec8061473ed6
LiChong <543281892@qq.com>
2016-09-11 14:12:09 +0800

    v-trans, page4

--- src/components/App.vue
+++ src/components/App.vue
@@ -86,53 +86,77 @@
 <template>
     <div class="app">
         <!--page1-->
-        <div class="page1" v-if="currentPage==1">
-            <img src="/img/bj01.jpg" alt="" class="bg">
-            <img class="text-1 pa" src="/img/capy01.png" alt="">
-            <img class="btn-1 pa" src="/img/btn01.png" @click="nextPage"/>
-        </div>
+        <img src="/img/bj01.jpg" alt="" class="bg" v-if="currentPage==1">
+        <img class="text-1 pa" src="/img/capy01.png" alt="" v-if="currentPage==1" transition="title-text" v-trans="{
+            transition:{transition:'all .7s ease .2s',opacity:1},
+            enter:{opacity:0,y:50},
+            leave:{opacity:0,transition:'all .1s ease'},
+                 ext:'%'}">
+        <img class="btn-1 pa" src="/img/btn01.png" @click="nextPage" v-if="currentPage==1"/>
         <!--/page1-->
 
         <!--page2-->
         <img src="/img/bj05.png" alt="" class="bg" v-if="currentPage>1 && currentPage<6">
-        <img class="light pa" src="/img/light.png" alt="" v-if="currentPage>1" v-anim="{animation:'bulb-blink 4s steps(7,start) infinite',frames:[
-                {},{opacity:1},{opacity:0.9},{opacity:0.7},{opacity:0.9},{opacity:1},{},'%'] }">
-        <img class="text-1 pa" src="/img/capy02.png" alt="" v-if="currentPage==2" transition="title-text" v-trans="{
-            transition:{transition:'all 1.2s ease .2s',opacity:1},
-            enter:{opacity:0,y:50},
-            leave:{opacity:0,'transition-duration':'.2s'},
-                 ext:'%'}">
+        <img class="light pa" src="/img/light.png" alt="" v-if="currentPage>1" v-anim="{animation:'bulb-blink 7s steps(9,start) infinite',frames:[
+                {},{},{opacity:1},{opacity:1},{opacity:0.7},{opacity:1},{opacity:1},{},{},'%'] }">
+        <img class="text-1 pa" src="/img/capy02.png" alt="" v-if="currentPage==2" transition="title-text">
         <img class="btn-1 pa" src="/img/btn02.png" @click="nextPage" v-if="currentPage==2"/>
 
         <!--/page2-->
 
         <!-- page3 -->
-        <img class="sofa pa" src="/img/sofa.png" alt="" v-if="currentPage>2 && currentPage<6">
-        <div class="page3" v-if="currentPage==3">
-            <img class="text-1 pa" src="/img/capy03.png" alt=""  v-if="currentPage==3" transition="title-text">
-            <img class="btn-1 pa" src="/img/btn03.png" @click="nextPage"/>
-        </div>
+        <img class="sofa pa" src="/img/sofa.png" alt="" v-if="currentPage>2 && currentPage<6" transition="sofa"
+             v-trans="{
+            transition:{transition:'all .9s ease',opacity:1},
+            enter:{opacity:.7,x:-50,y:50},
+            leave:{opacity:0,'transition-duration':'.2s'},
+                 ext:'%'}">
+        <img class="text-1 pa" src="/img/capy03.png" alt="" v-if="currentPage==3" transition="title-text-3" v-trans="{
+            transition:{transition:'all .7s ease .7s',opacity:1},
+            enter:{opacity:0,y:50},
+            leave:{opacity:0,transition:'all .1s ease'},
+                 ext:'%'}">
+        <img class="btn-1 pa" src="/img/btn03.png" @click="nextPage" v-if="currentPage==3"/>
         <!-- /page3-->
 
         <!--page4-->
-        <div class="page4To5" v-if="currentPage>3 && currentPage<6">
-            <img src="/img/curtain.png" alt="" class="curtain pa">
-            <img src="/img/curtain01.png" alt="" class="curtain01 pa">
-            <img src="/img/draw.png" alt="" class="draw pa">
-            <img src="/img/table.png" alt="" class="table pa">
-        </div>
-        <div class="page4" v-if="currentPage==4">
-            <img class="text-1 pa" src="/img/capy04.png" alt="">
-            <img class="btn-1 pa" src="/img/btn04.png" @click="nextPage"/>
-        </div>
+        <!--page4 to 5-->
+        <img src="/img/curtain.png" alt="" class="curtain pa" v-if="currentPage>3 && currentPage<6" transition="curtain"
+             v-trans="{
+            transition:{transition:'all .9s ease .5s',opacity:1},
+            enter:{opacity:0,x:50,y:50},
+            leave:{},
+                 ext:'%'}">
+        <img src="/img/curtain01.png" alt="" class="curtain01 pa" v-if="currentPage>3 && currentPage<6"
+             transition="curtain01" v-trans="{
+            transition:{transition:'all .7s ease .9s',opacity:1},
+            enter:{opacity:0,x:50},
+            leave:{},
+                 ext:'%'}">
+        <img src="/img/draw.png" alt="" class="draw pa" v-if="currentPage>3 && currentPage<6" transition="sofa">
+        <img src="/img/table.png" alt="" class="table pa" v-if="currentPage>3 && currentPage<6" transition="table"
+             v-trans="{
+            transition:{transition:'all .9s ease 1.5s',opacity:1},
+            enter:{opacity:0,x:-20,y:50},
+            leave:{},
+                 ext:'%'}">
+        <!--/page4 to 5-->
+        <img class="text-1 pa" src="/img/capy04.png" alt="" v-if="currentPage==4" transition="title-text-4" v-trans="{
+            transition:{transition:'all .7s ease 2.3s',opacity:1},
+            enter:{opacity:0,y:50},
+            leave:{opacity:0,transition:'all .1s ease'},
+                 ext:'%'}">
+        <img class="btn-1 pa" src="/img/btn04.png" @click="nextPage" v-if="currentPage==4"/>
         <!--/page4-->
 
         <!--page5-->
-        <div class="page5" v-if="currentPage==5">
-            <img src="/img/food.png" alt="" class="food pa">
-            <img class="text-1 pa" src="/img/capy05.png" alt="">
-            <img class="btn-1 pa" src="/img/btn05.png" @click="nextPage"/>
-        </div>
+        <img src="/img/food.png" alt="" class="food pa" v-if="currentPage==5" transition="food" v-trans="{
+            transition:{transition:'all .7s ease',opacity:1},
+            enter:{opacity:0,rotate:4,scale:.7},
+            leave:{opacity:0,transition:'all .1s ease'},
+                 ext:'%'}">
+        <img class="text-1 pa" src="/img/capy05.png" alt="" v-if="currentPage==5" transition="title-text-3">
+        <img class="btn-1 pa" src="/img/btn05.png" @click="nextPage" v-if="currentPage==5"/>
         <!--/page5-->
 
         <!--page6-->
