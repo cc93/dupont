a5f545e2e443af63f830a837b2f3e8bb99c99ddb
LiChong <543281892@qq.com>
2016-09-11 16:37:16 +0800

    auto-scale

--- src/components/App.vue
+++ src/components/App.vue
@@ -5,9 +5,17 @@
         position: relative;
     }
 
-    .bg {
+    .platform {
         width: 1334px;
         height: 750px;
+        position: absolute;
+    }
+
+    .bg {
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
         position: absolute;
         z-index: -10;
     }
@@ -85,127 +93,138 @@
 </style>
 <template>
     <div class="app">
-        <!--page1-->
-        <img src="/img/bj01.jpg" alt="" class="bg" v-if="currentPage==1">
-        <img class="text-1 pa" src="/img/capy01.png" alt="" v-if="currentPage==1">
-        <img class="btn-1 pa" src="/img/btn01.png" @click="nextPage" v-if="currentPage==1"/>
-        <!--/page1-->
+        <div class="platform" v-auto-scale="{width:1334,height:750}">
+            <!--page1-->
+            <img src="/img/bj01.jpg" alt="" class="bg" v-if="currentPage==1">
+            <img class="text-1 pa" src="/img/capy01.png" alt="" v-if="currentPage==1">
+            <img class="btn-1 pa" src="/img/btn01.png" @click="nextPage" v-if="currentPage==1"/>
+            <!--/page1-->
 
-        <!--page2-->
-        <img src="/img/bj05.png" alt="" class="bg" v-if="currentPage>1 && currentPage<6">
-        <img class="light pa" src="/img/light.png" alt="" v-if="currentPage>1" v-anim="{animation:'bulb-blink 5s linear infinite',frames:[
+            <!--page2-->
+            <img src="/img/bj05.png" alt="" class="bg" v-if="currentPage>1 && currentPage<6">
+            <img class="light pa" src="/img/light.png" alt="" v-if="currentPage>1" v-anim="{animation:'bulb-blink 5s linear infinite',frames:[
                 {},{},{opacity:1},{opacity:1,scaleX:1},{opacity:0.7,scaleX:0.95},{opacity:1,scaleX:1},{opacity:1},{},{},'%'] }">
-        <img class="text-1 pa" src="/img/capy02.png" alt="" v-if="currentPage==2" transition="title-text" v-trans="{
+            <img class="text-1 pa" src="/img/capy02.png" alt="" v-if="currentPage==2" transition="title-text" v-trans="{
             transition:{transition:'all .7s ease .2s',opacity:1},
             enter:{opacity:0,y:50},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <img class="btn-1 pa" src="/img/btn02.png" @click="nextPage" v-if="currentPage==2" transition="title-btn"  v-trans="{
+            <img class="btn-1 pa" src="/img/btn02.png" @click="nextPage" v-if="currentPage==2" transition="title-btn"
+                 v-trans="{
             transition:{transition:'opacity .7s ease .7s',opacity:1},
             enter:{opacity:0},
             leave:{opacity:0,transition:'opacity .1s ease'},
                  ext:'%'}"/>
 
-        <!--/page2-->
+            <!--/page2-->
 
-        <!-- page3 -->
-        <img class="sofa pa" src="/img/sofa.png" alt="" v-if="currentPage>2 && currentPage<6" transition="sofa"
-             v-trans="{
+            <!-- page3 -->
+            <img class="sofa pa" src="/img/sofa.png" alt="" v-if="currentPage>2 && currentPage<6" transition="sofa"
+                 v-trans="{
             transition:{transition:'all .9s ease',opacity:1},
             enter:{opacity:.7,x:-50,y:50},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <img class="text-1 pa" src="/img/capy03.png" alt="" v-if="currentPage==3" transition="title-text-3" v-trans="{
+            <img class="text-1 pa" src="/img/capy03.png" alt="" v-if="currentPage==3" transition="title-text-3"
+                 v-trans="{
             transition:{transition:'all .7s ease .7s',opacity:1},
             enter:{opacity:0,y:50},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <img class="btn-1 pa" src="/img/btn03.png" @click="nextPage" v-if="currentPage==3" transition="title-btn-3" v-trans="{
+            <img class="btn-1 pa" src="/img/btn03.png" @click="nextPage" v-if="currentPage==3" transition="title-btn-3"
+                 v-trans="{
             transition:{transition:'opacity .7s ease 1.4s',opacity:1},
             enter:{opacity:0},
             leave:{opacity:0,transition:'opacity .1s ease'},
                  ext:'%'}"/>
-        <!-- /page3-->
+            <!-- /page3-->
 
-        <!--page4-->
-        <!--page4 to 5-->
-        <img src="/img/curtain.png" alt="" class="curtain pa" v-if="currentPage>3 && currentPage<6" transition="curtain"
-             v-trans="{
+            <!--page4-->
+            <!--page4 to 5-->
+            <img src="/img/curtain.png" alt="" class="curtain pa" v-if="currentPage>3 && currentPage<6"
+                 transition="curtain"
+                 v-trans="{
             transition:{transition:'all .9s ease .5s',opacity:1},
             enter:{opacity:0,x:50,y:50},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <img src="/img/curtain01.png" alt="" class="curtain01 pa" v-if="currentPage>3 && currentPage<6"
-             transition="curtain01" v-trans="{
+            <img src="/img/curtain01.png" alt="" class="curtain01 pa" v-if="currentPage>3 && currentPage<6"
+                 transition="curtain01" v-trans="{
             transition:{transition:'all .7s ease .9s',opacity:1},
             enter:{opacity:0,x:50},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <img src="/img/draw.png" alt="" class="draw pa" v-if="currentPage>3 && currentPage<6" transition="sofa">
-        <img src="/img/table.png" alt="" class="table pa" v-if="currentPage>3 && currentPage<6" transition="table"
-             v-trans="{
+            <img src="/img/draw.png" alt="" class="draw pa" v-if="currentPage>3 && currentPage<6" transition="sofa">
+            <img src="/img/table.png" alt="" class="table pa" v-if="currentPage>3 && currentPage<6" transition="table"
+                 v-trans="{
             transition:{transition:'all .9s ease 1.5s',opacity:1},
             enter:{opacity:0,x:-20,y:50},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <!--/page4 to 5-->
-        <img class="text-1 pa" src="/img/capy04.png" alt="" v-if="currentPage==4" transition="title-text-4" v-trans="{
+            <!--/page4 to 5-->
+            <img class="text-1 pa" src="/img/capy04.png" alt="" v-if="currentPage==4" transition="title-text-4"
+                 v-trans="{
             transition:{transition:'all .7s ease 2.3s',opacity:1},
             enter:{opacity:0,y:50},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <img class="btn-1 pa" src="/img/btn04.png" @click="nextPage" v-if="currentPage==4" transition="title-btn-4" v-trans="{
+            <img class="btn-1 pa" src="/img/btn04.png" @click="nextPage" v-if="currentPage==4" transition="title-btn-4"
+                 v-trans="{
             transition:{transition:'opacity .7s ease 2.9s',opacity:1},
             enter:{opacity:0},
             leave:{opacity:0,transition:'opacity .1s ease'},
                  ext:'%'}"/>
-        <!--/page4-->
+            <!--/page4-->
 
-        <!--page5-->
-        <img src="/img/food.png" alt="" class="food pa" v-if="currentPage==5" transition="food" v-trans="{
+            <!--page5-->
+            <img src="/img/food.png" alt="" class="food pa" v-if="currentPage==5" transition="food" v-trans="{
             transition:{transition:'all .7s ease',opacity:1},
             enter:{opacity:0,rotate:20,scale:1.3},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <img class="text-1 pa" src="/img/capy05.png" alt="" v-if="currentPage==5" transition="title-text-3">
-        <img class="btn-1 pa" src="/img/btn05.png" @click="nextPage" v-if="currentPage==5" transition="title-btn-3"/>
-        <!--/page5-->
+            <img class="text-1 pa" src="/img/capy05.png" alt="" v-if="currentPage==5" transition="title-text-3">
+            <img class="btn-1 pa" src="/img/btn05.png" @click="nextPage" v-if="currentPage==5"
+                 transition="title-btn-3"/>
+            <!--/page5-->
 
-        <!--page6-->
-        <img src="/img/bj06.png" alt="" class="bg" v-if="currentPage>5" transition="fade-in" v-trans="{
+            <!--page6-->
+            <img src="/img/bj06.png" alt="" class="bg" v-if="currentPage>5" transition="fade-in" v-trans="{
             transition:{transition:'opacity .7s ease',opacity:1},
             enter:{opacity:0},
             leave:{opacity:0,transition:'opacity .1s ease'},
                  ext:'%'}">
-        <img class="text-1 pa" src="/img/capy06.png" alt="" v-if="currentPage==6" transition="title-text-3">
-        <img class="btn-1 pa" src="/img/btn06.png" @click="nextPage" v-if="currentPage==6" transition="title-btn-3"/>
-        <!--/page6-->
+            <img class="text-1 pa" src="/img/capy06.png" alt="" v-if="currentPage==6" transition="title-text-3">
+            <img class="btn-1 pa" src="/img/btn06.png" @click="nextPage" v-if="currentPage==6"
+                 transition="title-btn-3"/>
+            <!--/page6-->
 
-        <!--page7-->
-        <img src="/img/kongt.png" alt="" class="kongtiao pa" v-if="currentPage>6" transition="kongtiao" v-trans="{
+            <!--page7-->
+            <img src="/img/kongt.png" alt="" class="kongtiao pa" v-if="currentPage>6" transition="kongtiao" v-trans="{
             transition:{transition:'all .7s ease 0.2s',opacity:1},
-            enter:{opacity:0,x:30,y:10},
+            enter:{opacity:0,x:10,y:10},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <img src="/img/tv.png" alt="" class="tv pa" v-if="currentPage>6" transition="tv" v-trans="{
+            <img src="/img/tv.png" alt="" class="tv pa" v-if="currentPage>6" transition="tv" v-trans="{
             transition:{transition:'all .7s ease 0.7s',opacity:1},
             enter:{opacity:0,y:30},
             leave:{opacity:0,transition:'all .1s ease'},
                  ext:'%'}">
-        <div class="page7-mask pa" v-if="currentPage>6" transition="mask" v-trans="{
+            <div class="page7-mask pa" v-if="currentPage>6" transition="mask" v-trans="{
             transition:{transition:'opacity .7s ease 2.4s',opacity:1},
             enter:{opacity:0},
             leave:{opacity:0,transition:'opacity .1s ease'},
                  ext:'%'}">
-            <img src="/img/logo.png" alt="" class="logo pa">
-        </div>
-        <img src="/img/bj07.jpg" alt="" class="bg" style="z-index: 20" v-if="currentPage>6" transition="last-page" v-trans="{
+                <img src="/img/logo.png" alt="" class="logo pa">
+            </div>
+            <img src="/img/bj07.jpg" alt="" class="bg" style="z-index: 20" v-if="currentPage>6" transition="last-page"
+                 v-trans="{
             transition:{transition:'opacity .7s ease 4.4s',opacity:1},
             enter:{opacity:0},
             leave:{opacity:0,transition:'opacity .1s ease'},
-                 ext:'%'}" />
-        <!-- 延时后给背景加滤镜 -->
-        <!--/page7-->
+                 ext:'%'}"/>
+            <!--/page7-->
+
+        </div>
     </div>
 </template>
 <script>
@@ -271,6 +290,34 @@
                     var anim = Smart.Animations.create(name, frames);
                     this.el.style.animation = anim.name + ' ' + time + ' ' + timeFn + ' ' + loop;
                 }
+            },
+            'auto-scale': {
+                update: function (val) {
+                    var el = this.el;
+                    var width = val.width;
+                    var height = val.height;
+                    var ratio = width / height;
+                    var newWidth, newHeight, newX, newY, newScale;
+                    var resize = function () {
+                        var winWidth = window.innerWidth;
+                        var winHeight = window.innerHeight;
+                        var winRatio = winWidth / winHeight;
+                        if (ratio > winRatio) {
+                            newWidth = winWidth;
+                            newHeight = newWidth / ratio;
+                        } else {
+                            newHeight = winHeight;
+                            newWidth = newHeight * ratio;
+                        }
+                        newX = (winWidth - newWidth ) / 2;     //translateX
+                        newY = (winHeight - newHeight ) / 2;   //translateY
+                        newScale = newWidth / width;            //scaleX,scaleY
+                        var cssSmartObj = {x: newX, y: newY, scale: newScale, 'transform-origin': '0 0'};
+                        Smart.Css.smartCss(el, cssSmartObj, 'px');
+                    };
+                    resize();
+                    window.onresize = resize;
+                }
             }
         },
         data(){
@@ -280,57 +327,13 @@
             }
         },
         ready(){
-            this.autoScale(750, 1334);
         },
         methods: {
             nextPage(){
-                //total: 9 pages
                 if (this.currentPage <= 9) {
                     this.currentPage++;
                 }
-            },
-            toPage(page){
-                this.currentPage = page;
-            },
-            autoScale(width, height) {
-                //设计稿的长宽
-                var ratio = width / height;
-                var newWidth, newHeight, newX, newY, newScale;
-                var winWidth = window.innerWidth;
-                var winHeight = window.innerHeight;
-                var winRatio = winWidth / winHeight;
-                if (ratio > winRatio) {
-                    newWidth = winWidth;
-                    newHeight = newWidth / ratio;
-                } else {
-                    newHeight = winHeight;
-                    newWidth = newHeight * ratio;
-                }
-                newX = (winWidth - newWidth ) / 2;     //translateX
-                newY = (winHeight - newHeight ) / 2;   //translateY
-                newScale = newWidth / width;            //scaleX,scaleY
-                console.log('scale = ' + newScale);
-                this.appStyle = {transform: 'scale(' + newScale + ',' + newScale + ')' + ' translate3d(' + newX + 'px,' + newY + 'px,' + '0)'};
-                window.addEventListener('resize', function () {
-                    var winWidth = window.innerWidth;
-                    var winHeight = window.innerHeight;
-                    var winRatio = winWidth / winHeight;
-                    if (ratio > winRatio) {
-                        newWidth = winWidth;
-                        newHeight = newWidth / ratio;
-                    } else {
-                        newHeight = winHeight;
-                        newWidth = newHeight * ratio;
-                    }
-                    newX = (winWidth - newWidth ) / 2;     //translateX
-                    newY = (winHeight - newHeight ) / 2;   //translateY
-                    newScale = newWidth / width;            //scaleX,scaleY
-                    console.log('scale = ' + newScale);
-                    this.appStyle = {transform: 'scale(' + newScale + ',' + newScale + ')' + ' translate3d(' + newX + 'px,' + newY + 'px,' + '0)'};
-                }.bind(this));
-
             }
-
         }
     }
 </script>
